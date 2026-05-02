a88d1cbed6fabb38aa13596ec2fb2b7ddf014f4a
rrtt217 <wwq169@mail.ustc.edu.cn>
2026-05-03 00:32:28 +0800

    Revert "fix(wayland): some inputs events stalling others"
    This reverts commit 579d2b8ba8fd45199b88ac235af3394619950c04.

--- src/wl_window.c
+++ src/wl_window.c
@@ -1688,7 +1688,7 @@ static void handleEvents(double* timeout)
         while (wl_display_prepare_read(_glfw.wl.display) != 0)
         {
             if (wl_display_dispatch_pending(_glfw.wl.display) > 0)
-                event = GLFW_TRUE;
+                return;
         }
 
         // If an error other than EAGAIN happens, we have likely been disconnected
